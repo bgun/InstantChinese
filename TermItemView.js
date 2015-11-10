e8146989b0b212353f7d228efd8e3c3fdf336428
Ben Gundersen <ben@bengundersen.com>
2015-11-10 01:50:43 -0500

    term cards

--- TermItemView.js
+++ TermItemView.js
@@ -1,6 +1,7 @@
 import React from 'react-native';
 
 var {
+  Image,
   ListView,
   Text,
   View,
@@ -9,9 +10,16 @@ var {
 export default class TermItemView extends React.Component {
 
   render() {
+
+    let size = this.props.size;
+
     return (
-      <View>
-        <Text>{ this.props.term }</Text>
+      <View style={{ backgroundColor: '#000000', flex: 1, height: size }}>
+        <Image source={{ uri: this.props.term.image_url }} resizeMode='cover' style={{ position: 'absolute', width: size, height: size }} />
+        <View style={{ backgroundColor: 'transparent', justifyContent: 'center', alignItems: 'center', flex: 1, flexDireciton: 'column', width: size, height: size, position: 'absolute' }}>
+          <Text style={{ alignItems: 'center', color: '#FFFFFF', fontSize: 40, shadowColor: '#000000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.1, shadowRadius: 5 }}>{ this.props.term.term[0] }</Text>
+          <Text style={{ alignItems: 'center', color: '#FFFFFF', fontSize: 30 }}>{ this.props.term.term[1] }</Text>
+        </View>
       </View>
     );
   }
--- TermListView.js
+++ TermListView.js
@@ -12,17 +12,28 @@ export default class TermListView extends React.Component {
 
   constructor(props) {
     super();
-    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
-      dataSource: ds.cloneWithRows(props.data)
+      dataSource: ds.cloneWithRows(props.data),
+      layout: {
+        width: 100
+      }
     };
+  }
+
+  onLayout(event) {
+    this.setState({
+      layout: event.nativeEvent.layout
+    })
   }
 
   render() {
     return (
       <ListView
+        style={{ flex: 1, flexDirection: 'column' }}
+        onLayout={ this.onLayout.bind(this) }
         dataSource={ this.state.dataSource }
-        renderRow={ (rowData) => <TermItemView term={ rowData } /> }
+        renderRow={ (rowData) => <TermItemView term={ rowData } size={ this.state.layout.width } /> }
       />
     );
   }
